7c2bad1b420082550f04f02615993a6a7f053d5f
Nayoung <nayoung0066@gmail.com>
2021-03-20 00:41:37 +0900

    다이나믹 프로그래밍
    - 백준 1003

---
+++ fibonacci.py
@@ -0,0 +1,10 @@
+cnt0 = [1, 0]
+cnt1 = [0, 1]
+
+for i in range(2, 41):
+    cnt0.append(cnt0[i-1]+cnt0[i-2])
+    cnt1.append(cnt1[i-1]+cnt1[i-2])
+    
+for _ in range(int(input())):
+    n = int(input())
+    print(cnt0[n], cnt1[n])
